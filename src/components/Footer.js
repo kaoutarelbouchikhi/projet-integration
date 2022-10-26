099cac6ae0f7e81823c34789e6f7c55de04d5cd7
Madevp12 <108009070+Madevp12@users.noreply.github.com>
2022-10-26 13:50:00 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,45 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Logo from "../components/Logo";
+
+
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <a href="#"><img src="facebook.png" alt="facebook" /></a>
-            <a href="#"><img src="instagram.png" alt="instagram" /></a>
-            <a href="#"><img src="youtube.png" alt="youtube" /></a>
-            <p>© WEBH22L, all rights reserved</p>
-        </div>
+        <Container fluid
+            className="footer">
+            <Row>
+                <Col xs={{ span: 5, offset: 1 }} md={{ span: 3, offset: 3 }} >
+                    <Logo />
+                </Col>
+                <Col  xs={{ span: 5, offset: 1 }} md={{ span: 3, offset: 3 }}>
+                    <ul class="list-inline social-buttons">
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row>
+                <h6 class="text-center p-2 text-white"> © 2022 Copyright  Team E</h6>
+            </Row>
+        </Container>
     )
 }
 
 export default Footer;
+
