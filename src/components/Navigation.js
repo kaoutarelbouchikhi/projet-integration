891585631fd3d00900118a27c8c866bbf52d9a97
Madevp12 <108009070+Madevp12@users.noreply.github.com>
2022-10-26 13:51:00 -0400

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,33 +3,57 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Logo from "../components/Logo";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const navigation = () => {
   return (
     <Navbar
       collapseOnSelect
-      expand="lg"
-      bg=""
+      expand="xl"
       variant="dark"
       className="navigation"
+      
     >
       <Container>
-        <Navbar.Brand href="/accueil">
+        <Navbar.Brand href="/">
           <Logo />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="m-auto text-center">
             <Nav.Link href="/">Accueil</Nav.Link>
-            <Nav.Link href="/categorie">Categorie</Nav.Link>
-            <Nav.Link href="/fiche-film">Fiche film</Nav.Link>
-            <Nav.Link href="/fiche-acteur">Fiche acteur</Nav.Link>
-            <Nav.Link href="/recherche">Recherche</Nav.Link>
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              <Row>
+                <Col>
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2"> Aventure </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Animation</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">Comédie</NavDropdown.Item>
+                  
+                </Col>
+                <Col>
+                <NavDropdown.Item href="#action/3.4">Crime</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.1">Drame</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2"> Fantastique</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Historique</NavDropdown.Item>
+            
+                </Col>
+                <Col>
+                  <NavDropdown.Item href="#action/3.4">Horreur</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">Mystère</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">Romance</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">Suspense</NavDropdown.Item>
+                </Col>
+              </Row>
+            </NavDropdown>
             <Nav.Link href="/a-propos">À propos</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link href="/connexion">Connexion</Nav.Link>
+          <Nav className="text-center">
+            <Nav.Link href="/recherche">Recherche</Nav.Link>
+            <Nav.Link href="/caroussel">Connexion</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
